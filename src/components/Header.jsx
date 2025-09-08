0e3e96bbf5518518fd2374bb31697271bb44acf3
sabirkazi135 <sabirkazi135@gmail.com>
2025-09-08 22:05:16 +0530

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <>
+      <div>
+        <div>
+          <span>🚀</span>
+          <span>AstroWind</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
